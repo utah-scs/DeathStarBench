8f532d6d35ec460cecc3eb488d9fd22bd3426655
Tian Zhang <tian@kv.microservices.sandstorm.emulab.net>
2020-04-15 00:56:01 -0600

    Add script to install dependances.

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install libz-dev
+sudo apt-get install luarocks
+sudo luarocks install luasocket
+sudo apt-get install python3.8
+sudo apt-get install docker.io
+sudo apt-get install docker-compose
+sudo apt-get install python3-pip
+pip3 install aiohttp
