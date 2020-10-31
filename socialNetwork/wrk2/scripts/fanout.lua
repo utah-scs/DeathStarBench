262677ceca410159fee543fabce88ccf3d79de27
Tian Zhang <tian@client.nanoservices.sandstorm.emulab.net>
2020-10-30 23:57:00 -0600

    Add fanout microbench.

---
+++ socialNetwork/wrk2/scripts/fanout.lua
@@ -0,0 +1,17 @@
+require "socket"
+math.randomseed(socket.gettime()*1000)
+math.random(); math.random(); math.random()
+
+local function test()
+  local method = "POST"
+  local path = "/fanout/func"
+  local headers = {}
+  local body = ""
+  headers["MS"] = "1"
+  headers["NUM"] = "8"
+  return wrk.format(method, path, headers, body)
+end
+
+request = function()
+    return test()
+end
