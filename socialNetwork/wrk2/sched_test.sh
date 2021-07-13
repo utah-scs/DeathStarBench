b75090fa4ebdcd04982a9bef269fef7cfae07a42
Tian Zhang <tian@client.nanoservices.sandstorm.emulab.net>
2021-07-12 19:42:13 -0600

    Modify sched_test script.

--- socialNetwork/wrk2/sched_test.sh
+++ socialNetwork/wrk2/sched_test.sh
@@ -1,69 +1,69 @@
 #!/bin/sh
 
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output1 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output1 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output2 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output2 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output3 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output3 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output4 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output4 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output5 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output5 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output6 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output6 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output7 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output7 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output8 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output8 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output9 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output9 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output10 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output10 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output11 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output11 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output12 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output12 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output13 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output13 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output14 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output14 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output15 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output15 &
 sleep 0.05
-./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 16 > output16 &
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_10ms.lua http://10.0.1.1:11211/sched_bench/run_10ms -R 20 > output16 &
 sleep 0.05
 
 
 
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
-#./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
-#sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
+./wrk -t 1 -c 1 -d 100 -L -s ./scripts/sched_bench_100ms.lua http://10.0.1.1:11211/sched_bench/run_100ms -R 4 &
+sleep 0.1
